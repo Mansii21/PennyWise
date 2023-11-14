cf46115a5fd711cf0ccce7bb88270d597b659174
Mansi Singh <mansisingh45556@gmail.com>
2023-11-14 22:51:40 +0530

    User Api and controllers

--- Expense_tracker/Backend/app.js
+++ Expense_tracker/Backend/app.js
@@ -1,34 +1,36 @@
-const express=require("express")
-const {db}=require("./db/db")
-const cors=require("cors");
-const {readdirSync}=require('fs')
-const app=express()
+const express = require("express");
+const { db } = require("./db/db");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+const app = express();
+const { errorHandler, notFound } = require("../Backend/middleware/error");
+require("dotenv").config();
 
-require('dotenv').config();
-
-const PORT=process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //routes
-readdirSync('./routes').map((route)=>
-    app.use('/api/v1',require('./routes/'+route))
-)
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
-app.get('/',(req,res)=>{
-    res.send('hiiii')
-    //console.log(" hihihii ")
-    //alert("hi")
-}
-)
+app.get("/login", (req, res) => {
+  res.send("hiiii");
+  //console.log(" hihihii ")
+  //alert("hi")
+});
+//error handler
+app.use(notFound);
+app.use(errorHandler);
 
-const server=()=>{
-    db()
-    app.listen(PORT,()=>{
-        console.log(" listening to port :",PORT)
-    })
-    //console.log("You are listening to port :",PORT);
-}
-server()
+const server = () => {
+  db();
+  app.listen(PORT, () => {
+    console.log(" listening to port :", PORT);
+  });
+  //console.log("You are listening to port :",PORT);
+};
+server();
--- Expense_tracker/Backend/routes/transactions.js
+++ Expense_tracker/Backend/routes/transactions.js
@@ -1,12 +1,38 @@
-const { addExpense, getExpense, deleteExpense } = require("../controllers/expense");
-const {addIncome,getIncomes, deleteIncome}=require("../controllers/income")
-const router=require('express').Router();
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
+const {
+  userRegisterCtrl,
+  loginUserCtrl,
+  fetchUsersCtrl,
+  userProfileCtrl,
+  updateUserCtrl,
+  deleteUsersCtrl,
+  fetchUserDetailsCtrl,
+} = require("../controllers/user");
+const authMiddleware = require("../middleware/authMiddleware");
+const router = require("express").Router();
 
-
-router.post('/add-income',addIncome)
-    .get('/get-incomes',getIncomes)
-    .delete('/delete-income/:id',deleteIncome)
-    .post('/add-expense',addExpense)
-    .get('/get-expense',getExpense)
-    .delete('/delete-expense/:id',deleteExpense)
-module.exports=router
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expense", getExpense)
+  .delete("/delete-expense/:id", deleteExpense)
+  .post("/register", userRegisterCtrl)
+  .post("/login", loginUserCtrl)
+  .get("/", authMiddleware, fetchUsersCtrl)
+  .get("/users", fetchUsersCtrl)
+  .get("/profile/", authMiddleware, userProfileCtrl)
+  .put("/:id", authMiddleware, updateUserCtrl)
+  .delete("/:id", deleteUsersCtrl)
+  .get("/:id", fetchUserDetailsCtrl);
+module.exports = router;
